3417ce5cf333860f39ea6f9ed71a0cd8bef4f587
Chris Smith <chrisronsmith@gmail.com>
2020-03-16 08:52:16 -0400

    Break up main js file

---
+++ files.js
@@ -0,0 +1,43 @@
+export const initFiles = (handleFile) => { 
+  const dropArea = document.getElementById('drop-area')
+
+  ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+  })
+
+  function preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  ;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+  })
+
+  ;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+  })
+
+  function highlight(e) {
+    dropArea.classList.add('highlight')
+  }
+
+  function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+  }
+
+  dropArea.addEventListener('drop', handleDrop, false)
+  dropArea.addEventListener('change', (e) => handleFiles(e.target.files))
+
+  function handleDrop(e) {
+    let dt = e.dataTransfer
+    let files = dt.files
+
+    handleFiles(files)
+  }
+
+  function handleFiles(files) {
+    const arr = [...files]
+    arr.forEach(handleFile)
+  }
+}
--- smpl.js
+++ smpl.js
@@ -1,44 +1,6 @@
-const dropArea = document.getElementById('drop-area')
+import { initFiles } from './files.js'
+initFiles(handleFile)
 
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, preventDefaults, false)
-})
-
-function preventDefaults(e) {
-  e.preventDefault()
-  e.stopPropagation()
-}
-
-;['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
-})
-
-function highlight(e) {
-  dropArea.classList.add('highlight')
-}
-
-function unhighlight(e) {
-  dropArea.classList.remove('highlight')
-}
-
-dropArea.addEventListener('drop', handleDrop, false)
-dropArea.addEventListener('change', (e) => handleFiles(e.target.files))
-
-function handleDrop(e) {
-  let dt = e.dataTransfer
-  let files = dt.files
-
-  handleFiles(files)
-}
-
-function handleFiles(files) {
-  const arr = [...files]
-  arr.forEach(handleFile)
-}
 
 const audioContext = new AudioContext()
 const audioBuffers = []
@@ -103,53 +65,7 @@ function handleFile(file) {
   reader.readAsArrayBuffer(file)
 }
 
-const playableKeys = {
-  "0": { "index": 9, "row": 3 },
-  "1": { "index": 0, "row": 3 },
-  "2": { "index": 1, "row": 3 },
-  "3": { "index": 2, "row": 3 },
-  "4": { "index": 3, "row": 3 },
-  "5": { "index": 4, "row": 3 },
-  "6": { "index": 5, "row": 3 },
-  "7": { "index": 6, "row": 3 },
-  "8": { "index": 7, "row": 3 },
-  "9": { "index": 8, "row": 3 },
-  "z": { "index": 0, "row": 0 },
-  "x": { "index": 1, "row": 0 },
-  "c": { "index": 2, "row": 0 },
-  "v": { "index": 3, "row": 0 },
-  "b": { "index": 4, "row": 0 },
-  "n": { "index": 5, "row": 0 },
-  "m": { "index": 6, "row": 0 },
-  ",": { "index": 7, "row": 0 },
-  ".": { "index": 8, "row": 0 },
-  "/": { "index": 9, "row": 0 },
-  "a": { "index": 0, "row": 1 },
-  "s": { "index": 1, "row": 1 },
-  "d": { "index": 2, "row": 1 },
-  "f": { "index": 3, "row": 1 },
-  "g": { "index": 4, "row": 1 },
-  "h": { "index": 5, "row": 1 },
-  "j": { "index": 6, "row": 1 },
-  "k": { "index": 7, "row": 1 },
-  "l": { "index": 8, "row": 1 },
-  ";": { "index": 9, "row": 1 },
-  "'": { "index": 10, "row": 1 },
-  "q": { "index": 0, "row": 2 },
-  "w": { "index": 1, "row": 2 },
-  "e": { "index": 2, "row": 2 },
-  "r": { "index": 3, "row": 2 },
-  "t": { "index": 4, "row": 2 },
-  "y": { "index": 5, "row": 2 },
-  "u": { "index": 6, "row": 2 },
-  "i": { "index": 7, "row": 2 },
-  "o": { "index": 8, "row": 2 },
-  "p": { "index": 9, "row": 2 },
-  "[": { "index": 10, "row": 2 },
-  "]": { "index": 11, "row": 2 },
-  "-": { "index": 10, "row": 3 },
-  "=": { "index": 11, "row": 3 }
-}
+import { playableKeys } from './playableKeys.js'
 
 const getBufSrc = (audioBuffer) => {
   const sourceNode = audioContext.createBufferSource()
@@ -163,7 +79,7 @@ const getBufSrc = (audioBuffer) => {
 }
 
 const noteIndex = (char, rowOffset = 3) => {
-  playableKey = playableKeys[char]
+  const playableKey = playableKeys[char]
   if (!playableKey) return
   return playableKey.index + playableKey.row * rowOffset
 }
